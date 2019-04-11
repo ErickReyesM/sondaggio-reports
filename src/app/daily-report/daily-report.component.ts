28268fa1646ac4ceebe860200cd237f471c53850
ereyesm <ereyes@intersysconsulting.com>
2019-04-11 14:47:45 -0500

    creating chart data object

--- src/app/daily-report/daily-report.component.ts
+++ src/app/daily-report/daily-report.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
 import * as CanvasJS from '../../assets/canvasjs.min';
 import * as firebase from 'firebase';
 
@@ -13,66 +12,51 @@ export class DailyReportComponent implements OnInit {
   collection:string = 'userInput';
   surveyId:string = 'HgtFAndYZm1uYHdcEPk8';
   today:Date = new Date();
-  todayData:any[] = [];
+  dataTime:number[] = [];
   milliSecondsInADay:number = 86400000;
 
-  constructor() { 
-        /*let todayNow = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
-          let dayInSurvey = new Date(docSnapShot.data().created.toDate().getFullYear(), 
-          docSnapShot.data().created.toDate().getMonth(),
-          docSnapShot.data().created.toDate().getDate());
-          if ( dayInSurvey.getTime() == todayNow.getTime() ){
-            this.todayData.push(docSnapShot);
-          }*/
-  }
+  constructor() { }
 
   ngOnInit() {
-    let docsS:firebase.firestore.QueryDocumentSnapshot[];
 
-    let chart = new CanvasJS.Chart("daily-chartContainer", {
+    let dataObject = {
       animationEnabled: true,
       exportEnabled: true,
-      title: {
-        text: "Reporte Diario"
-      },
-      data: [{
-        type: "column",
-        dataPoints: [
-          { y: 71, label: "Apple" },
-          { y: 55, label: "Mango" },
-          { y: 50, label: "Orange" },
-          { y: 65, label: "Banana" },
-          { y: 95, label: "Pineapple" },
-          { y: 68, label: "Pears" },
-          { y: 28, label: "Grapes" },
-          { y: 34, label: "Lychee" },
-          { y: 14, label: "Jackfruit" }
-        ]
-      }]
+      title: { text: "Reporte Diario" },
+      data:[
+        {
+          type: "column",
+          name: this.today.toDateString(),
+          showInLegend: true,
+          dataPoints: []
+        }
+      ]
+    };
+
+    let todayNow = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
+    let dayInSurvey:Date;
+    this.getDocsFromFireBase(this.collection, this.surveyId).then((querySnapshot) => {
+      querySnapshot.docs.forEach((doc) => {
+        dayInSurvey = 
+        new Date(doc.data().created.toDate().getFullYear(), doc.data().created.toDate().getMonth(), doc.data().created.toDate().getDate());
+        if ( dayInSurvey.getTime() == todayNow.getTime() ){
+          //console.log(doc.data().created.toDate().getTime());
+          //console.log(doc.data().input);
+          dataObject.data[0].dataPoints.push({ label: doc.data().created.toDate().getHours(), y: 4 });
+        }
+      });
+    }).catch( (err) => {
+      //TODO
     });
-      
+
+    console.log(dataObject);
+    let chart = new CanvasJS.Chart("daily-chartContainer", dataObject);
+
     chart.render();
-
-    
-    docsS = this.getDocsFromFireBase();
-    console.log(docsS);
-
   }
 
-  private getDocsFromFireBase():firebase.firestore.QueryDocumentSnapshot[]{
-
-    let docs:firebase.firestore.QueryDocumentSnapshot[] = [];
-
-    firebase.firestore().collection(this.collection).where('surveyID', '==', this.surveyId).get().then(
-      (querySnapShot) => {
-        docs = docs.concat();
-      }
-    ).catch(
-      (err) => {
-        console.log(err);
-      }
-    )
-    return docs;
+  private getDocsFromFireBase(collection:string, surveyId:string):Promise<firebase.firestore.QuerySnapshot>{
+    return firebase.firestore().collection(collection).where('surveyID', '==', surveyId).get();
   }
 
 }
